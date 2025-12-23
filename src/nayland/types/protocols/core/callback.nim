de2c6eab46f2c6e12683bc4f51e22e3e54af51c3
xTrayambak <xtrayambak@gmail.com>
2025-12-23 16:11:29 +0530

    fix: some callback fixes

--- src/nayland/types/protocols/core/callback.nim
+++ src/nayland/types/protocols/core/callback.nim
@@ -6,7 +6,7 @@ import pkg/nayland/bindings/libwayland, pkg/nayland/bindings/protocols/core
 type
   CallbackObj = object
     handle: ptr wl_callback
-    listener: wl_callback_listener
+    listener: ptr wl_callback_listener
 
   CallbackCallback* = proc(callback: Callback, obj: pointer, callbackData: uint32)
     ## The ultimate callback. Fear it.
@@ -15,6 +15,7 @@ type
 
   CallbackPayloadObj = object
     obj*: pointer
+    cbObj*: ptr CallbackObj
     cb*: CallbackCallback
 
   CallbackPayload* = ref CallbackPayloadObj
@@ -22,19 +23,30 @@ type
   Callback* = ref CallbackObj
 
 proc listen*(callback: Callback, obj: pointer, cb: CallbackCallback) =
+  # what in the world is this....
+  if callback.listener == nil:
+    callback.listener =
+      cast[ptr wl_callback_listener](alloc(sizeof(wl_callback_listener)))
+
   callback.listener.done = proc(
       data: pointer, cb: ptr wl_callback, cbdata: uint32
   ) {.cdecl.} =
     let payload = cast[ptr CallbackPayloadObj](data)
-    payload.cb(Callback(handle: cb), payload.obj, cbdata)
+    payload.cb(cast[Callback](payload.cbObj), payload.obj, cbdata)
 
-  let payload = CallbackPayload(obj: obj, cb: cb)
-  discard wl_callback_add_listener(
-    callback.handle, callback.listener.addr, cast[ptr CallbackPayloadObj](payload)
-  )
+  let payload = cast[ptr CallbackPayloadObj](alloc(sizeof(CallbackPayloadObj)))
+
+  payload.cb = cb
+  payload.cbObj = cast[ptr CallbackObj](callback)
+  payload.obj = obj
+
+  discard wl_callback_add_listener(callback.handle, callback.listener, payload)
 
 proc destroy*(callback: Callback) =
   wl_callback_destroy(callback.handle)
+
+proc implantListener*(source: Callback, dest: Callback) =
+  dest.listener = source.listener
 
 func newCallback*(handle: ptr wl_callback): Callback =
   Callback(handle: handle)
